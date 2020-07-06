bfa765aa69e2681be4ce92eeddcfe74f2c5f6ee5
MarioTorresLopez <66542079+MarioTorresLopez@users.noreply.github.com>
2020-07-06 18:06:03 -0500

    Certificado de seguridad

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@
 |
 */
 //$config['base_url'] = 'http://localhost/Superrecarga/';
-$config['base_url'] = 'http://superrecarga.com.mx/';
+$config['base_url'] = 'https://superrecarga.com.mx/';
 //$config['base_url'] = 'http://3.101.13.211/SuperRecargaMeta/';
 //$config['base_url'] = 'http://cidtai.uteq.edu.mx/simposio_nano/';
 
