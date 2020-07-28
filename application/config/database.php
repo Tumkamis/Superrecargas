31a415abe1cbb135ba8eeda0abbee54a5ee58d84
MarioTorresLopez <apacan99@gmail.com>
2020-07-27 20:11:58 -0500

    Cambio aws

--- application/config/database.php
+++ application/config/database.php
@@ -49,26 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 
-$db['default']['hostname'] = 'superrecarga.com.mx';
-$db['default']['username'] = 'superrec_root';
-$db['default']['password'] = 'diatel2020';
-$db['default']['database'] = 'superrec_superrecargafundacion';
-$db['default']['dbdriver'] = 'mysqli';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = FALSE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
-
-//$db['default']['hostname'] = '54.183.11.183';
-//$db['default']['username'] = 'root';
+//$db['default']['hostname'] = 'superrecarga.com.mx';
+//$db['default']['username'] = 'superrec_root';
 //$db['default']['password'] = 'diatel2020';
-//$db['default']['database'] = 'superrecargafundacion';
+//$db['default']['database'] = 'superrec_superrecargafundacion';
 //$db['default']['dbdriver'] = 'mysqli';
 //$db['default']['dbprefix'] = '';
 //$db['default']['pconnect'] = TRUE;
@@ -80,6 +64,22 @@ $db['default']['stricton'] = FALSE;
 //$db['default']['swap_pre'] = '';
 //$db['default']['autoinit'] = TRUE;
 //$db['default']['stricton'] = FALSE;
+
+$db['default']['hostname'] = '54.183.11.183';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'diatel2020';
+$db['default']['database'] = 'superrecargafundacion';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = FALSE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
 
 
 
