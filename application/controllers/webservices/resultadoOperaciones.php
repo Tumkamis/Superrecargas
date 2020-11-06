a5c7e12bdec949d0bcdf861f46b8d6cb90b2b53c
MarioTorresLopez <apacan99@gmail.com>
2020-11-06 11:00:07 -0600

    Recibir JSON

--- application/controllers/webservices/resultadoOperaciones.php
+++ application/controllers/webservices/resultadoOperaciones.php
@@ -20,26 +20,27 @@ class resultadoOperaciones extends CI_Controller{
     }
     
     public function index() {
-        $data = json_decode($_POST['d'], true);
+        //$data = json_decode($_POST['d'], true);
+        $_POST = json_decode(file_get_contents('php://input'), true);
         $arr_msjc = array();
-        $arr_msjc['certComercioProveedor'] = $data['cadenaInformacion']['certComercioProveedor'];
-        $arr_msjc['celularCliente'] = $data['cadenaInformacion']['celularCliente'];
-        $arr_msjc['digitoVerificadorCliente'] = $data['cadenaInformacion']['digitoVerificadorCliente'];
-        $arr_msjc['nombreCliente'] = $data['cadenaInformacion']['nombreCliente'];
-        $arr_msjc['claveInstitucionCliente'] = $data['cadenaInformacion']['claveInstitucionCliente'];
-        $arr_msjc['tipoCuentaCliente'] = $data['cadenaInformacion']['tipoCuentaCliente'];
-        $arr_msjc['numeroCuentaCliente'] = $data['cadenaInformacion']['numeroCuentaCliente'];
-        $arr_msjc['idMensajeCobro'] = $data['cadenaInformacion']['idMensajeCobro'];
-        $arr_msjc['concepto'] = $data['cadenaInformacion']['concepto'];
-        $arr_msjc['monto'] = $data['cadenaInformacion']['monto'];
-        $arr_msjc['claveRastreo'] = $data['cadenaInformacion']['claveRastreo'];
-        $arr_msjc['resultadoMensajeCobro'] = $data['cadenaInformacion']['resultadoMensajeCobro'];
-        $arr_msjc['horaSolicitudMensajeCobro'] = $data['cadenaInformacion']['horaSolicitudMensajeCobro'];
-        $arr_msjc['horaProcMensajeCobro'] = $data['cadenaInformacion']['horaProcMensajeCobro'];
-        $arr_msjc['certBdeM'] = $data['cadenaInformacion']['certBdeM'];
-        $arr_msjc['horaEnvioMensaje'] = $data['cadenaInformacion']['horaEnvioMensaje'];
+        $arr_msjc['certComercioProveedor'] = $_POST['cadenaInformacion']['certComercioProveedor'];
+        $arr_msjc['celularCliente'] = $_POST['cadenaInformacion']['celularCliente'];
+        $arr_msjc['digitoVerificadorCliente'] = $_POST['cadenaInformacion']['digitoVerificadorCliente'];
+        $arr_msjc['nombreCliente'] = $_POST['cadenaInformacion']['nombreCliente'];
+        $arr_msjc['claveInstitucionCliente'] = $_POST['cadenaInformacion']['claveInstitucionCliente'];
+        $arr_msjc['tipoCuentaCliente'] = $_POST['cadenaInformacion']['tipoCuentaCliente'];
+        $arr_msjc['numeroCuentaCliente'] = $_POST['cadenaInformacion']['numeroCuentaCliente'];
+        $arr_msjc['idMensajeCobro'] = $_POST['cadenaInformacion']['idMensajeCobro'];
+        $arr_msjc['concepto'] = $_POST['cadenaInformacion']['concepto'];
+        $arr_msjc['monto'] = $_POST['cadenaInformacion']['monto'];
+        $arr_msjc['claveRastreo'] = $_POST['cadenaInformacion']['claveRastreo'];
+        $arr_msjc['resultadoMensajeCobro'] = $_POST['cadenaInformacion']['resultadoMensajeCobro'];
+        $arr_msjc['horaSolicitudMensajeCobro'] = $_POST['cadenaInformacion']['horaSolicitudMensajeCobro'];
+        $arr_msjc['horaProcMensajeCobro'] = $_POST['cadenaInformacion']['horaProcMensajeCobro'];
+        $arr_msjc['certBdeM'] = $_POST['cadenaInformacion']['certBdeM'];
+        $arr_msjc['horaEnvioMensaje'] = $_POST['cadenaInformacion']['horaEnvioMensaje'];
         
-        $arr_msjc['selloDigital'] = $data['selloDigital'];
+        $arr_msjc['selloDigital'] = $_POST['selloDigital'];
         
         //echo $arr_msjc['selloDigital'];
         post_msj($arr_msjc);
