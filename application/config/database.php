ed3ed33ca195388444daabfb81b2fba418f96596
Tumkamis <82539574+Tumkamis@users.noreply.github.com>
2021-06-04 14:11:47 -0500

    Update database.php
    cambios actuales

--- application/config/database.php
+++ application/config/database.php
@@ -65,9 +65,9 @@ $active_record = TRUE;
 //$db['default']['autoinit'] = TRUE;
 //$db['default']['stricton'] = FALSE;
 
-$db['default']['hostname'] = '54.183.11.183';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'diatel2020';
+$db['default']['hostname'] = '54.176.233.121';
+$db['default']['username'] = 'Diatel';
+$db['default']['password'] = 'diatel';
 $db['default']['database'] = 'superrecargafundacion';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
